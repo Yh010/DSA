56e14e835a2b8104b3840a25af51e48a0f701b14
Yash Hegde <77537293+Yh010@users.noreply.github.com>
2022-05-11 16:36:06 +0530

    Update changing case in string.cpp

--- string/changing case in string.cpp
+++ string/changing case in string.cpp
@@ -9,5 +9,17 @@ int main()
         a[i] -= 32 ;
     }
     
-    cout<< a ;
+   cout<< a <<"\n";
+    
+    //upper to lower -lower to upper
+    char b[]="DaTaStRuCt" ;
+    for(int i=0; b[i] !='\0' ;i++){
+        if(b[i] >= 'A' && b[i] <= 'Z'){
+            b[i] += 32 ;
+        }else if(b[i] >='a' && b[i] <= 'z'){
+            b[i] -= 32 ;
+        }
+    }
+    
+    cout<<b ;
 }
