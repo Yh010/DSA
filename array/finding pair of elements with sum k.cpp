10c1d95a7f913cff0e567aaaeb7451cffbfabfd7
Yash Hegde <77537293+Yh010@users.noreply.github.com>
2022-05-11 11:44:43 +0530

    Add files via upload

---
+++ array/finding pair of elements with sum k.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main()
+{
+   int arr[10]={3,6,8,8,10,12,15,15,15,20};
+   int max=arr[0];
+   for(int i=1;i<10;i++){
+       if(arr[i]>max){
+           max=arr[i];
+       }
+   }
+   int h[max+1]={0};
+   int sum= 10 ;
+   int pairs=0;
+   for(int i=0;i<10;i++){
+       if(h[sum-arr[i]]>0){
+           pairs++ ;
+           cout<< arr[i] <<" "<< sum-arr[i] <<"\n" ;
+       }
+       h[arr[i]]++ ;
+   }
+   cout<<pairs;
+   
+   
+}
+//doubt : why 15 and -5 is getting counted and 10 is not counted
